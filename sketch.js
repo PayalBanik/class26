4b1eda0a69feb83e28485984418cbb4eb49a7fdb
PayalBanik <payalbanik@yahoo.in>
2021-05-12 12:44:48 +0530

    created a platform

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ const Bodies = Matter.Bodies;
 
 var engine, world;
 var box1, pig1;
-
+var platform;
 function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
@@ -12,6 +12,7 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
+    platform = new Ground(150,305,300,170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -54,4 +55,5 @@ function draw(){
     log5.display();
 
     bird.display();
+    platform.display();
 }
